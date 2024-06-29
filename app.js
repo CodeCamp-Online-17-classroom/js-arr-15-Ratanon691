ac1cbd243640b10f8f63e2fce3a33019f039b14c
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-30 00:07:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+function first(arr, n){
+  if(n == 1 || n == undefined){
+    return arr[0]
+  }else{
+    return arr.slice(0, n)
+  }
+}
+console.log(first([7, 9, 0, -2])); // 7
+console.log(first([], 3)); // []
+console.log(first([7, 9, 0, -2], 3)); // [7, 9 ,0]
